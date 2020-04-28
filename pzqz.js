4940a82e72d713a340ed0735d866d2cb8b64a492
kiwi707 <56808142+kiwi707@users.noreply.github.com>
2020-04-28 15:47:21 +0800

    Create pzqz.js

---
+++ pzqz.js
@@ -0,0 +1,21 @@
+/*
+Quantumult X:
+#拍照取字unlock Vip
+^http:\/\/pay\.maoqilai\.com\/get_user_info url script-response-body pzqz.js
+app下载链接（国区）：https://apps.apple.com/cn/app/%E6%8B%8D%E7%85%A7%E5%8F%96%E5%AD%97-%E5%B0%B1%E6%98%AF%E5%A5%BD%E7%94%A8/id1249414505
+MITM=pay.maoqilai.com
+Tg频道：https://t.me/jmkji
+*/
+var obj = JSON.parse($response.body);
+ obj={
+  "paid_service_list" : [
+    {
+      "service_id" : 7,
+      "start_time" : 1588058243,
+      "end_time" : 3742787843,
+      "service_name" : "连续包月月度VIP会员"
+    }
+  ],
+};
+$done({body: JSON.stringify(obj)});
+//
